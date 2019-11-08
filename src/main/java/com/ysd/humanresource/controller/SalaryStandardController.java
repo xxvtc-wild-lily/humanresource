a28a86744576a39ffc2717f614a99d3a75e8325f
SleepBoy3 <1059809641@qq.com>
2019-11-08 14:13:21 +0800

    薪酬管理
    完成薪酬标准提交

--- src/main/java/com/ysd/humanresource/controller/SalaryStandardController.java
+++ src/main/java/com/ysd/humanresource/controller/SalaryStandardController.java
@@ -2,7 +2,10 @@ package com.ysd.humanresource.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.ysd.humanresource.entity.SalaryStandard;
 import com.ysd.humanresource.service.SalaryStandardService;
+
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +13,10 @@ public class SalaryStandardController {
 	
 	@Autowired
 	private SalaryStandardService salaryStandardService;
+	
+	@PostMapping("/submitSalaryStandard")
+	public Integer submitSalaryStandard(SalaryStandard salaryStandard) {
+		return salaryStandardService.insertSalaryStandard(salaryStandard);
+	}
 	
 }
--- src/main/java/com/ysd/humanresource/service/SalaryStandardService.java
+++ src/main/java/com/ysd/humanresource/service/SalaryStandardService.java
@@ -1,5 +1,14 @@
 package com.ysd.humanresource.service;
 
-public interface SalaryStandardService {
+import com.ysd.humanresource.entity.SalaryStandard;
 
+public interface SalaryStandardService {
+	
+	/**
+	 * 添加薪酬标准
+	 * @param salaryStandard 包含薪酬标准信息的实体类
+	 * @return 受影响的行数
+	 */
+	Integer insertSalaryStandard(SalaryStandard salaryStandard);
+	
 }
