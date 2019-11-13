3c8e87fd3e4f95f6d4ea0793d5183fdc00473ca6
SleepBoy3 <1059809641@qq.com>
2019-11-13 09:36:08 +0800

    薪酬管理
    完成薪酬标准复核

--- src/main/java/com/ysd/humanresource/dao/SalaryStandardMapper.java
+++ src/main/java/com/ysd/humanresource/dao/SalaryStandardMapper.java
@@ -1,5 +1,8 @@
 package com.ysd.humanresource.dao;
 
+import java.util.List;
+
+import com.ysd.humanresource.entity.Pagination;
 import com.ysd.humanresource.entity.SalaryStandard;
 
 public interface SalaryStandardMapper {
@@ -10,5 +13,33 @@ public interface SalaryStandardMapper {
 	 * @return 受影响的行数
 	 */
 	Integer insertSalaryStandard(SalaryStandard salaryStandard);
+	
+	/**
+	 * 根据薪酬名称查找该薪酬标准是否存在
+	 * @param salaryStandard 包含薪酬标准信息的实体类
+	 * @return 查询到的符合条件的行数
+	 */
+	Integer selectCountBySalaryStandardName(SalaryStandard salaryStandard);
+	
+	/**
+	 * 查询未审核的薪酬标准
+	 * @param pagination 包含分页信息的实体类
+	 * @return 查询到的未审核的薪酬信息
+	 */
+	List<SalaryStandard> selectAllUncheckSalaryStandard(Pagination pagination);
+	
+	/**
+	 * 根据薪酬名称和id查找该薪酬标准是否存在
+	 * @param salaryStandard 包含薪酬标准信息的实体类
+	 * @return 查询到的符合条件的行数
+	 */
+	Integer selectCountBySalaryStandardNameAndId(SalaryStandard salaryStandard);
+	
+	/**
+	 * 通过薪酬标准审核后根据id修改薪酬标准
+	 * @param salaryStandard 包含薪酬标准信息的实体类
+	 * @return 受影响的行数
+	 */
+	Integer updateSalaryStandardById(SalaryStandard salaryStandard);
 	
 }
--- src/main/java/com/ysd/humanresource/service/impl/SalaryStandardServiceImpl.java
+++ src/main/java/com/ysd/humanresource/service/impl/SalaryStandardServiceImpl.java
@@ -1,9 +1,12 @@
 package com.ysd.humanresource.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ysd.humanresource.dao.SalaryStandardMapper;
+import com.ysd.humanresource.entity.Pagination;
 import com.ysd.humanresource.entity.SalaryStandard;
 import com.ysd.humanresource.enums.SalaryStandardStatusEnum;
 import com.ysd.humanresource.service.SalaryStandardService;
@@ -29,6 +32,31 @@ public class SalaryStandardServiceImpl implements SalaryStandardService {
 		salaryStandard.setSs_vehicleSubsidy(MoneyJudgeUtil.judge(salaryStandard.getSs_vehicleSubsidy()));
 		
 		return salaryStandardMapper.insertSalaryStandard(salaryStandard);
+	}
+	
+	@Override
+	public Integer selectCountBySalaryStandardName(SalaryStandard salaryStandard) {
+		// TODO Auto-generated method stub
+		return salaryStandardMapper.selectCountBySalaryStandardName(salaryStandard);
+	}
+
+	@Override
+	public List<SalaryStandard> selectAllUncheckSalaryStandard(Pagination pagination) {
+		// TODO Auto-generated method stub
+		return salaryStandardMapper.selectAllUncheckSalaryStandard(pagination);
+	}
+	
+	@Override
+	public Integer selectCountBySalaryStandardNameAndId(SalaryStandard salaryStandard) {
+		// TODO Auto-generated method stub
+		return salaryStandardMapper.selectCountBySalaryStandardNameAndId(salaryStandard);
+	}
+
+	@Override
+	public Integer updateSalaryStandardById(SalaryStandard salaryStandard) {
+		// TODO Auto-generated method stub
+		salaryStandard.setSs_aduitStatus(SalaryStandardStatusEnum.SALARYSTANDARDPASSEDADUIT.getCode());
+		return salaryStandardMapper.updateSalaryStandardById(salaryStandard);
 	}
 	
 }
